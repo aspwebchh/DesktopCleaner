7bd17a789a91660872eb85e668f9e7505bade1de
DH-PC\Administrator <aspwebchh@hotmail.com>
2017-09-24 12:17:06 +0800

    [add]条件rapidjson

--- logic/clear_desktop.cpp
+++ logic/clear_desktop.cpp
@@ -11,6 +11,11 @@
 #include "common.h"
 #include "export.h"
 
+#include "rapidjson\document.h"
+#include "rapidjson\writer.h"
+#include "rapidjson\stringbuffer.h"
+
+
 
 using namespace std;
 
@@ -131,9 +136,27 @@ vector<SummaryItem> moveFiles(vector<FileItem> files, string dirPath) {
 }
 
 
+void testJSON()
+{
+	using namespace rapidjson;
+	const char* json = "{\"project\":\"rapidjson\",\"stars\":10}";
+	Document d;
+	d.Parse(json);
+	// 2. 利用 DOM 作出修改。
+	Value& s = d["stars"];
+	s.SetInt(s.GetInt() + 1);
+	// 3. 把 DOM 转换（stringify）成 JSON。
+	StringBuffer buffer;
+	Writer<StringBuffer> writer(buffer);
+	d.Accept(writer);
+	// Output {"project":"rapidjson","stars":11}
+	std::cout << buffer.GetString() << std::endl;
+}
 int main() {
 
+	testJSON();
 
+	cin.get();
 
 	auto path = getDesktopPath() + "\\test_desktop";
 	auto files = getFiles(path);
